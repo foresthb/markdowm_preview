7f29ebfe01e91c91d1e472e808591f874f464919
ihb <gusqja4515@naver.com>
2018-10-12 17:09:15 +0900

    App.js
    컴포넌트들을 파일 하나로 통합하고 마크다운 기능 추가 : react-marked-markdown을 설치해야 함

--- App.js
+++ App.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Editor from './Editor';
+import {  MarkdownPreview  } from 'react-marked-markdown';
+
+const placeholder = "Preview";
+const textBoxStyle = {
+  width: 800,
+  height: 200
+}
+
+const PreviewBoxStyle = {
+  border: '1.5px solid skyblue',
+  color: 'yellowgreen'
+}
+
+class Preview extends Component {
+  render() {
+    return (
+      <div className="Prv" style={PreviewBoxStyle}>
+        <MarkdownPreview value={this.props.text} />
+      </div>
+    )
+  }
+}
+
+class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mkd: placeholder
+    }
+    this.inputChange = this.inputChange.bind(this);
+  }
+  inputChange(event) {
+    this.setState({
+      mkd: event.target.value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <textarea type="text" style={textBoxStyle} onChange={this.inputChange} />
+        <Preview text={this.state.mkd} />
+      </div>
+    )
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
   render() {
     return (
       <div className="App">
